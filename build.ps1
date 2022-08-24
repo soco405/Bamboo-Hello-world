846b11a9e557f9d420532d804a55ee6e71cf0adf
John Harris <bdow@johnharp1f3md6r.lan>
2022-08-24 14:46:55 -0400

    changed to java

--- build.ps1
+++ build.ps1
@@ -1,17 +1,3 @@
-#! /usr/bin/env bash
-
-$JAVA_VERSION='(Get-Command java | Select-Object -ExpandProperty Version).tostring()'
-$PACKAGE_DIR="com/thalesgroup"
-$PACKAGE_NAME="com.thalesgroup"
-$CLASS_NAME="App"
-$FULL_PACKAGE_NAME="${PACKAGE_NAME}.${CLASS_NAME}"
-$BUILD_DIR="app/bin"
-$SRC_DIR="app/src"
-$RES_DIR="app/res"
-$LARGE_FILE="${bamboo.shortPlanName}-build-${bamboo.buildNumber}.file"
-$FILE_SIZE=2048; #2MB file
-#$FILE_SIZE=2048000; #Uncomment for 2GB file
-
 #Dending on the size of the file you want to create, you can swap out varibles
 $twoMBfile=2048576
 $oneGBfile=1024288000
@@ -20,10 +6,7 @@ $twoGBfile=2048576000
 #Creates a large file with plan and build identifiers.
 $out = new-object byte[] $twoMBfile; 
 (new-object Random).NextBytes($out); 
-[IO.File]::WriteAllBytes("$RES_DIR\${LARGE_FILE}", $out)
-Write-Output ""
-
-
+[IO.File]::WriteAllBytes('app\res\${bamboo.LargeFile}', $out)
 
 # Show Java version
 if ($JAVA_VERSION -gt 1)
@@ -34,26 +17,21 @@ else
 {
 exit 0
 }
-Write-Output ""
-
-# Build the class definition
+#Building Class Definitio
 Write-Output 'Generating class definitions'
-set-location "$SRC_DIR"
-javac -d "${MY_PWD}\${BUILD_DIR}/classes' '${PACKAGE_DIR}\${CLASS_NAME}.java"
-Write-Output ""
+Set-Location -Path '.\app\src' -PassThru
+javac -d "..\bin\classes\" ".\com\thalesgroup\App.java"
 
 # Build the jar
 Write-Output 'Packaging jar file'
-set-location "${BUILD_DIR}\classes";
-jar cfe "${BUILD_DIR}\jar\${CLASS_NAME}.jar" ${FULL_PACKAGE_NAME} "${PACKAGE_DIR}\${CLASS_NAME}.class" "..\../res\${LARGE_FILE}";
-Write-Output ""
+Set-Location -Path '.\app\bin\classes' -PassThru
+jar cfe "..\jar\App.jar" com.thalesgroup.App "com\thalesgroup\App.class" 
 
-# Show contents of jar
-Write-Output 'Contents of jar:';
-jar tf "${BUILD_DIR}\jar/${CLASS_NAME}.jar"
-Write-Output ""
-
-# Run the jar
-Write-Output 'Running of jar:'
-Write-Output ""
-java -jar "${BUILD_DIR}\jar\${CLASS_NAME}.jar"
+#need to add large file
+Write-Output 'add large file'
+Set-Location -Path '.\app' -PassThru
+jar -uf .\bin\jar\App.jar res\${bamboo.LargeFile}
+#show contents of Jar
+jar tf ".\bin\jar\App.jar"
+Write-Output 'Running jar file'
+java -jar ".\bin\jar\App.jar"
